ae74b20d15fd8b8aa1166fb3af1029a9964fad61
Robson Lunardelli <robson@l9s.dev>
2019-08-24 20:25:38 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # laravel-wsl
-Laravel Development Environment on WSL
+
+The script and instructions below will setup an environment for Laravel development on your Windows Systems for Linux.
+
+**Shout-Out**: Some of the instructions here and the tasks for the bootstrap script were taken from this nice [tutorial](https://dev-squared.com/2018/05/15/getting-started-with-laravel-development-on-windows-subsystem-for-linux-with-vs-code-a-complete-guide/).
+
+## Setup
+
+### Enable WSL
+
+Follow the instructions in this [documentation](https://docs.microsoft.com/en-us/windows/wsl/install-win10). Once you get WSL enabled, go ahead and install Ubuntu.
+
+### Bootstrap script
+
+Once your WSL is up and running, download the bootstrap script and run it on your WSL. It will perform the following tasks:
+
+1. Update Linux
+2. Install PHP 7.3 and configure it
+3. Install MariaDB
+4. Install Redis
+5. Install composer and valet
+6. Install and configure vim
+7. Configure xdebug
+
+### Docroot
+
+Create a docroot where your code will exist. Since the Windows filesystem can be access from the WSL `/mnt/`, create a directory shared between the two spaces and symlinked it for your home directory in WSL.
+
+```
+ln -s /mnt/c/Users/<username>/<code_directory> ~/code
+```
+
+Run `valet` from within this new directory:
+
+```
+valet park
+```
+
+### DNS
+
+Download and install [Acrylic DNS Proxy](http://mayakron.altervista.org/wikibase/show.php?id=AcrylicHome) on your Windows machine so you can wildcard domains for local development.
+
+From the Acrylic folder in the start menu, click on "**Edit Acrylic Hosts file**". Add the following line to the bottom:
+
+```
+127.0.0.1   *.test
+```
+
+Change your network adapter DNS settings to point to 127.0.0.1. You can follow the instructions [here](https://www.windowscentral.com/how-change-your-pcs-dns-settings-windows-10).
+
+**Note**: Now any requests to `<whatever>.test` will be routed to the appropriate named Valet folder.
+
+### Init Laravel
+
+In WSL, navigate to the your code directory and the run the following:
+
+```
+composer create-project --prefer-dist laravel/laravel blog
+```
+
+Composer will create a `blog` folder will all the boilerplate. You should be able to access it at: ``http://blog.test``.
+
+### VSCode and PHP
+
+To get all the VS Code goodness to work with PHP on your Windows box, you need to download PHP and configure VS Code to use it.
+
+1. Download PHP and explode the package somewhere on your Windows machine
+2. Edit your VS Code settings and point the `php.validate.executablePath` setting to the path of the `php.exe` you just exploded:
+```
+"php.validate.executablePath":"C:\\Users\\<username>\\php\\php.exe"
+```
+3. Restart VS Code
+4. Install some PHP packages on your VS Code:
+   * PHP Intelephense
+   * PHP IntelliSense
---
+++ src/bootstrap.sh
@@ -0,0 +1,155 @@
+#! /bin/bash
+#
+#==============================================================================
+#
+###     Laravel Dev Environment Bootstrap
+#
+#==============================================================================
+
+PHP="7.3"
+
+#
+# format output
+#
+function output ()
+{
+local HOSTNAME=$(hostname)
+GREEN="\e[1;32m"
+YELLOW="\e[1;33m"
+RESET="\e[0m"
+echo -e "${YELLOW}==> ${GREEN}$HOSTNAME: ${RESET}$@ ..."
+}
+
+#
+# Boostrap
+#
+output "Update OS"
+sudo apt-get update -y &> /dev/null
+sudo apt-get upgrade -y &> /dev/null
+
+output "Install PHP and supporting libraries"
+sudo apt-get install -y php-xdebug php-fpm php-mysql php$PHP-cli php$PHP-curl php$PHP-mbstring php$PHP-xml php$PHP-zip php$PHP-intl curl git unzip php-cli network-manager libnss3-tools jq xsel &> /dev/null
+
+output "Download and install composer"
+cd ~
+curl -sS https://getcomposer.org/installer -o composer-setup.php &> /dev/null
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer &> /dev/null
+
+output "Configure and update composer"
+mkdir ~/.composer
+sudo cat > ~/.composer/composer.json << EOF
+{
+    "require": {
+    "valeryan/valet-wsl": "dev-master"
+    },
+    "repositories": [
+    {
+        "type": "vcs",
+        "url": "git@github.com:valeryan/valet-wsl.git"
+    }
+    ]
+}
+EOF
+sudo chown -R $USER ~/.composer
+composer global update &> /dev/null
+
+cat >> ~/.bashrc << EOF
+export PATH="$PATH:$HOME/.composer/vendor/bin"
+EOF
+
+output "Install valet"
+~/.composer/vendor/bin/valet install &> /dev/null
+
+output "Install Mariadb Server"
+sudo apt-get install -qq mariadb-server &> /dev/null
+
+sudo debconf-set-selections <<< "maria-db mysql-server/root_password password "
+sudo debconf-set-selections <<< "maria-db mysql-server/root_password_again password "
+
+sudo sed -i "s/bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf
+
+MYSQL=`which mysql`
+
+Q1="use mysql;"
+Q2="update user set plugin='' where User='root';"
+Q3="update user set host='%' where User='root';"
+Q4="flush privileges;"
+SQL="${Q1}${Q2}${Q3}${Q4}"
+
+sudo $MYSQL -uroot -e "$SQL"
+sudo service mysql restart
+
+output "Install Redis Server"
+sudo apt-get install -y redis-server &> /dev/null
+sudo service redis-server start
+
+output "Create startup script"
+cat > ~/start-services.sh << EOF
+#!/bin/bash
+
+if ps ax | grep -v grep | grep 'php-fpm' > /dev/null
+    then
+        echo 'FPM is running'
+    else
+        sudo service php$PHP-fpm start
+fi
+
+if ps ax | grep -v grep | grep 'nginx' > /dev/null
+    then
+        echo 'Nginx is running'
+    else
+        sudo service nginx start
+fi
+
+if ps ax | grep -v grep | grep 'mysql' > /dev/null
+    then
+        echo 'MySQL is running'
+    else
+        sudo service mysql start
+fi
+
+if ps ax | grep -v grep | grep 'redis-server' > /dev/null
+    then
+        echo 'Redis is running'
+    else
+        sudo service redis-server start
+fi
+EOF
+
+chmod +x ~/start-services.sh
+
+output "Install and configure vim"
+sudo apt-get install -y vim &> /dev/null
+mkdir -p ~/.vim/colors
+curl -o ~/.vim/colors/gruvbox.vim -O https://raw.githubusercontent.com/morhetz/gruvbox/master/colors/gruvbox.vim &> /dev/null
+cat >> ~/.vimrc << EOF
+set number
+syntax enable
+set background=dark
+colorscheme gruvbox
+set mouse=a
+
+if &term =~ '256color'
+" disable Background Color Erase (BCE) so that color schemes
+" render properly when inside 256-color tmux and GNU screen.
+" see also http://snk.tuxfamily.org/log/vim-256color-bce.html
+set t_ut=
+endif
+EOF
+
+echo "alias vi=vim" >> ~/.bashrc
+source ~/.bashrc
+
+output "Configure xdebug"
+cat >> /etc/php/$PHP/fpm/conf.d/20-xdebug.ini << EOF
+xdebug.default_enable=1
+xdebug.remote_enable=1
+xdebug.remote_connect_back=1
+xdebug.remote_port = 9001
+xdebug.scream=0
+xdebug.cli_color=1
+xdebug.show_local_vars=1
+xdebug.remote_autostart=1
+EOF
+
+sudo service php$PHP-fpm restart
